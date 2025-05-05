779eb059b20b689c4ca086c1f513e41f7038fc19
altughf <altughf@gmail.com>
2025-05-05 18:32:25 +0300

    Controllers & Models
    Routes can access controllers with use api end-points.
    And Controllers can use models for any interaction with database.

--- src/engine/Controllers/TodoController.php
+++ src/engine/Controllers/TodoController.php
@@ -2,12 +2,22 @@
 
 class TodoController {
 
+    private $todoModel_instance;
+
+    public function __construct(){
+
+        require_once APPROOT . '/Models/TodoModel.php';
+        $this->todoModel_instance = new TodoModel;
+    }
+
     public function index(){
 
         // DEFAULT LIST # OR # ANALYZE QUERY
 
+        $data = $this->todoModel_instance->todosModel();
+
         http_response_code(200);
-        echo json_encode(['status' => 'success','information' => 'TODO List']);
+        echo json_encode(['status' => 'success','information' => $data]);
 
     }
 
@@ -15,51 +25,86 @@ class TodoController {
 
         // OPEN FROM DATABASE
 
+        $data = $this->todoModel_instance->openModel($id);
+
         // http_response_code(400);
         http_response_code(200);
-        echo json_encode(['status' => 'success','information' => 'OPEN' . $id]);
+        echo json_encode(['status' => 'success','item' => $data]);
 
     }
 
     public function create(){
 
-        $input = json_decode(file_get_contents('php://input'), true);
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $todo_item = [
+
+            'title'                 => trim($data['todo-title']),
+            'description'           => trim($data['todo-description']),
+            'status'                => trim($data['todo-status']),
+            'priority'              => trim($data['todo-priority']),
+            'due_date'              => trim($data['todo-due-time']),
+        ];
+
+        $insert_ok = $this->todoModel_instance->createModel($todo_item);
 
         // ADD TO DATABASE > SUCCESS # OR # ERROR RESPONSE
 
         // http_response_code(400);
         http_response_code(201);
-        echo json_encode(['status' => 'success','information' => 'Add Todo', 'data' => $input]);
+        echo json_encode(['status' => 'success','information' => 'Add Todo', 'data' => $data]);
 
     }
 
     public function update($id){
 
-        $input = json_decode(file_get_contents('php://input'), true);
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $todo_item = [
+
+            'id'                    => $id,
+            'title'                 => trim($data['todo-title']),
+            'description'           => trim($data['todo-description']),
+            'status'                => trim($data['todo-status']),
+            'priority'              => trim($data['todo-priority']),
+            'due_date'              => trim($data['todo-due-time']),
+        ];
+
+        $update_ok = $this->todoModel_instance->updateModel($todo_item);
 
         // UPDATE TO DATABASE > SUCCESS # OR # ERROR RESPONSE
 
         // http_response_code(400);
         http_response_code(200);
-        echo json_encode(['status' => 'success','information' => 'UPDATE' . $id, 'data' => $input]);
+        echo json_encode(['status' => 'success','information' => 'UPDATE' . $id, 'data' => $data]);
 
     }
 
     public function updateStatus($id){
 
-        $input = json_decode(file_get_contents('php://input'), true);
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $todo_item = [
+
+            'id'                    => $id,
+            'status'                => trim($data['todo-status']),
+        ];
+
+        $update_ok = $this->todoModel_instance->updateStatusModel($todo_item);
 
         // UPDATE TO DATABASE > SUCCESS # OR # ERROR RESPONSE
 
         // http_response_code(400);
         http_response_code(200);
-        echo json_encode(['status' => 'success','information' => 'UPDATE STATUS' . $id, 'data' => $input]);
+        echo json_encode(['status' => 'success','information' => 'UPDATE STATUS' . $id, 'data' => $data]);
 
     }
 
     public function delete($id){
 
         // DELETE FROM DATABASE > SUCCESS # OR # ERROR RESPONSE
+
+        $this->todoModel_instance->deleteModel($id);
 
         // http_response_code(400);
         http_response_code(200);
--- src/engine/Models/TodoModel.php
+++ src/engine/Models/TodoModel.php
@@ -2,7 +2,146 @@
 
 class TodoModel {
 
-    /* PREPARE */
+    private $databaseInstance;
+
+    public function __construct(){
+
+        $this->databaseInstance = new databaseTasks;
+    }
+
+    public function createModel($todo_item){
+
+        $this->databaseInstance->query(
+
+            'INSERT INTO todos(title,description,status,priority,due_date) VALUES(:t_title,:t_description,:t_status,:t_priority,:t_due_date)'
+
+        );
+
+        $this->databaseInstance->bind(':t_title',$todo_item['title']);
+        $this->databaseInstance->bind(':t_description',$todo_item['description']);
+        $this->databaseInstance->bind(':t_status',$todo_item['status']);
+        $this->databaseInstance->bind(':t_priority',$todo_item['priority']);
+        $this->databaseInstance->bind(':t_due_date',$todo_item['due_date']);
+
+        if($this->databaseInstance->execute()){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public function updateModel($todo_item){
+
+        $this->databaseInstance->query(
+
+            'UPDATE todos
+            SET title = :t_title, description = :t_description, status = :t_status, priority = :t_priority, due_date = :t_due_date
+            WHERE todos.id = :todo_item_id'
+
+        );
+
+        $this->databaseInstance->bind(':todo_item_id',$todo_item['id']);
+        $this->databaseInstance->bind(':t_title',$todo_item['title']);
+        $this->databaseInstance->bind(':t_description',$todo_item['description']);
+        $this->databaseInstance->bind(':t_status',$todo_item['status']);
+        $this->databaseInstance->bind(':t_priority',$todo_item['priority']);
+        $this->databaseInstance->bind(':t_due_date',$todo_item['due_date']);
+
+        if($this->databaseInstance->execute()){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public function updateStatusModel($todo_item){
+
+        $this->databaseInstance->query(
+
+            'UPDATE todos
+            SET status = :t_status
+            WHERE todos.id = :todo_item_id'
+
+        );
+
+        $this->databaseInstance->bind(':todo_item_id',$todo_item['id']);
+        $this->databaseInstance->bind(':t_status',$todo_item['status']);
+
+        if($this->databaseInstance->execute()){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public function openModel($todo_item_id){
+
+        $this->databaseInstance->query(
+
+            'SELECT todos.id, todos.title, todos.description, todos.status, todos.priority, todos.due_date, todos.created_at, todos.updated_at, todos.deleted_at
+            FROM todos
+            WHERE todos.id = :todo_item_id'
+
+        );
+
+        $this->databaseInstance->bind(':todo_item_id',$todo_item_id);
+
+        $openTodo_item = $this->databaseInstance->single();
+
+        if($this->databaseInstance->rowCount() > 0){
+
+            return $openTodo_item;
+
+        } else {
+
+            go_direction('404');
+        }
+
+    }
+
+    public function deleteModel($todo_item_id){
+
+        $this->databaseInstance->query('DELETE FROM todos WHERE todos.id = :todo_item_id');
+
+        $this->databaseInstance->bind(':todo_item_id',$todo_item_id);
+
+        if($this->databaseInstance->execute()){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public function todosModel(){
+
+        $this->databaseInstance->query(
+
+            'SELECT todos.id, todos.title, todos.description, todos.status, todos.priority, todos.due_date, todos.created_at, todos.updated_at, todos.deleted_at
+            FROM todos'
+
+        );
+
+        $todos = $this->databaseInstance->resultSet();
+
+        return $todos;
+
+    }
 
 }
 
